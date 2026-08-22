d9ecb7023806ea4c539214a4e4fccc0dc3b00265
unknown <bisyrulhafiahmad@gmail.com>
2026-08-22 16:16:23 +0700

    Fix upload middleware imports

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getDb } = require('../db/connection');
 const { requireAuth } = require('../middleware/auth');
-const upload = require('../middleware/upload');
+const { upload } = require('../middleware/upload');
 const { put } = require('@vercel/blob');
 const fs = require('fs');
 
